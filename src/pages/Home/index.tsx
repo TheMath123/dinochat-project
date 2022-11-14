93576c6d99f00205caf055f09ae103f7061e87a8
Matheus P Agostinho <contact@matheuspa.me>
2022-11-14 13:59:06 -0300

    refac: remove button execute

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,11 @@
 import { Chat } from '../../components/Chat';
-import { useMessages } from '../../hooks/useMessages';
 import styles from '../../styles/home.module.scss';
 
 export default function Home() {
-  const { messages } = useMessages();
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <Chat />
-        <button onClick={() => console.log(messages)}>Execute</button>
       </div>
     </div>
   );
