b1bb6d1f12aab869cba9970c6ab61bd7ffc55483
Matheus P Agostinho <contact@matheuspa.me>
2022-12-15 13:54:17 -0300

    fix: remove credentials

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,11 +1,22 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5555', {
-  reconnectionDelayMax: 1000,
+const socket = io(import.meta.env.VITE_API_URL ?? 'http://localhost:5555', {
+  path: '/chat-connection/',
+  reconnectionDelayMax: 10000,
+  // withCredentials: true,
 });
 
 socket.on('connect', () => {
   console.log('[IO] Connect - A new connection has been established');
+});
+
+socket.on('connect', () => {
+  console.log('[IO] Connect - A new connection has been established');
+});
+
+socket.on('connect_error', err => {
+  console.error(`connect_error -> ${err.message}`);
+  // console.error(err);
 });
 
 export { socket };
