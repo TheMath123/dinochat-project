d3f9d6f44b1e50559d098e0e8cfa6166c69980dc
Matheus P Agostinho <contact@matheuspa.me>
2023-03-17 16:55:49 -0300

    chore: migrate to pnpm

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -6,4 +6,21 @@ const socket = io(import.meta.env.VITE_API_URL ?? 'http://localhost:3100', {
   transports: ['websocket'],
 });
 
+socket.on('connect', () => {
+  console.log('[IO] Connect - A new connection has been established');
+});
+
+socket.on('connect', () => {
+  console.log('[IO] Connect - A new connection has been established');
+});
+
+socket.on('chat.message', message => {
+  console.log('[IO] Message Recieve', message);
+});
+
+socket.on('connect_error', err => {
+  console.error(`connect_error -> ${err.message}`);
+  // console.error(err);
+});
+
 export { socket };
