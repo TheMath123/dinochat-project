d6a0b6effc12dd55e1a18df4490782c3bc473654
Alpha <alpha.vylly12@gmail.com>
2023-03-17 15:32:12 -0300

    chore: re-add reconnectionDelayMax: 10000

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 
 const socket = io(import.meta.env.VITE_API_URL ?? 'http://localhost:3100', {
   path: '/chat-connection/',
+  reconnectionDelayMax: 10000,
   transports: ['websocket'],
 });
 
